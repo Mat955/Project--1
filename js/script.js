331d896868f7bfdd444eaf52333e1e189a2bfdad
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-02-20 19:56:44 +0100

    Add responsive popups all

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function toggleMenu(visible) {
-    document.querySelector('.sidebar').classList.toggle('show', visible)
+    document.querySelector('.sidebar').classList.toggle('show1', visible)
     document.querySelector('.sidebar-small').classList.toggle('big', visible)
     document.querySelector('.btn').classList.toggle('hide', visible)
 }
@@ -7,4 +7,53 @@ function toggleMenu(visible) {
 document.querySelector('.icons-ham').addEventListener('click', function (e) {
     e.preventDefault();
     toggleMenu();
-});
+})
+
+
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        closeModal();
+    })
+})
+
+document.querySelector('#overlay').addEventListener('click', function (e) {
+    if (e.target === this) {
+        closeModal();
+    }
+})
+
+
+document.addEventListener('keyup', function (e) {
+    if (e.keyCode === 27) {
+        closeModal();
+    }
+})
+
+
+function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function (modal) {
+        modal.classList.remove('show')
+    })
+    document.querySelector('#overlay').classList.add('show')
+    document.querySelector(modal).classList.add('show')
+}
+
+document.querySelector('.top-bar .profile').addEventListener('click',
+    function () {
+        openModal('#myModal');
+    });
+
+document.querySelector('.top-bar .icon-quit').addEventListener('click',
+    function () {
+        openModal('#myModal-quit');
+    });
+
+document.querySelector('.sidebar .icon-postback').addEventListener('click',
+    function () {
+        openModal('#myModal-message');
+    });
